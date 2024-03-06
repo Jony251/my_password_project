fff54679a6c1e670f381cd5287546cb40d6e08f8
jony251 <nevgeny90@gmail.com>
2024-03-06 14:43:58 +0200

    first password proj

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import module_for_pass
+from random import randint
 
-numbers_list = module_for_pass.input_father()
-print("Your password", module_for_pass.user_num_in_out_txt(numbers_list))
+numbers_list = module_for_pass.random_number_list()
+print("Hello your password are:", module_for_pass.user_num_in_out_txt(numbers_list))
--- module_for_pass.py
+++ module_for_pass.py
@@ -1,52 +1,31 @@
-def get_user_number() -> int:
+from random import randint
+
+
+def random_number_list() -> list:
     """
-    Function gets a 5-digits number
-    :raises ValueError This exception is thrown when the user's number is not numeric
-    :return: Getting user's 5-digits number
+    Function builds list of 5-digit numbers
+    :return: list with length of 6 (6 number in list)
     """
-    try:
-        return int(input("Please enter a 5-digit number that is bigger than 11111, lower than 66666: "))
-    except ValueError:
-        raise ValueError("Your input is not a valid 5-digit number!")
+    rnd_list = []
+    for _ in range(6):
+        rnd_num = 0
+        for i in range(0, 5):
+            rnd = randint(1, 6)
+            rnd_num += rnd * (10 ** i)
 
-
-def check_5_digits() -> int:
-    '''
-    Function checks if the number is 5-digits number
-    if not the function asks user to input another input
-    :return: 5-digits number
-    '''
-    n = get_user_number()
-    while not (11110 < n < 66667):
-        n = get_user_number()
-
-    return n
-
-
-def input_father() -> list:
-    """
-    Function doesn't get anything
-    :return: a list with tree numbers, every number 5-digits
-    """
-    numbers_list = []
-    for _ in range(3):
-        try:
-            input_number = check_5_digits()
-            numbers_list.append(input_number)
-        except ValueError as e:
-            print("Error:", e)
-    return numbers_list
+        rnd_list.append(rnd_num)
+    return rnd_list
 
 
 def user_num_in_out_txt(list_of_nums) -> str:
     """
-    Function gets list of number with length 3
+    Function gets list of number with length 6
     :list_of_nums: a 3-number list
     :return: string of three words without spaces
     """
     out_str = ""
     number_digits = len(list_of_nums)
-    if (number_digits != 3):
+    if (number_digits != 6):
         return "The list aren't correct something got wrong"
     else:
         for numbers in list_of_nums:
